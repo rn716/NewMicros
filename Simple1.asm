6c3f3a4b8e8a0d9e3af7938f1f2734acf0040784
ep1616 <ep1616@skbl-422-013.ph.ic.ac.uk>
2018-10-15 11:00:47 +0100

    Counter with cascaded subroutines to delay by 255*255.

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,36 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw 	0xff
+	movwf	TRISD, ACCESS	    
 	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movwf	0x06, ACCESS
 	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+loop	movff 	0x06, PORTC
+	incf 	0x06, F, ACCESS
+	movlw   0xff
+        movwf   0x20, ACCESS
+	call    subrout, 0
+	nop
+
+test	movf    PORTD, W
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+
 	
+	
+				    ; a delay subroutine
+subrout movwf   0x30, ACCESS
+	call    subrou2, 0
+	nop
+	decfsz  0x20, F, ACCESS
+        bra     subrout
+        return  0
+	
+subrou2 decfsz  0x30, F, ACCESS
+        bra     subrou2
+        return  0
+
 	end
