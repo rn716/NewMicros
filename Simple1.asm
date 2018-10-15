4b0bd47ac4ef2c63a79231ff098532f67a1dfc90
ep1616 <ep1616@skbl-422-013.ph.ic.ac.uk>
2018-10-15 12:38:52 +0100

    output data to PORTE

--- Simple1.asm
+++ Simple1.asm
@@ -9,15 +9,15 @@
 	; ******* Programme FLASH read Setup Code ****  
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
-	movlw 	0x0
-	movwf	TRISC, ACCESS	    ; Port C all outputs
 	goto	start
 	; ******* My data and where to put it in RAM *
 myTable db	0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07
 	constant myArray=0x400	; Address in RAM for data
 	constant counter=0x10	; Address of counter variable
 	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
+start 	movlw 	0x0
+	movwf	TRISE, ACCESS	; Port E all outputs
+	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
 	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
@@ -27,8 +27,8 @@ start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM
 	movlw	.8		; 8 bytes to read
 	movwf 	counter		; our counter register
 loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move read data from TABLAT to (FSR0), increment FSR0
-	movff 	PORTC
+	movff	TABLAT, PORTE; move read data from TABLAT to (FSR0), increment FSR0
+	;movff 	FSR0, PORTC
 	decfsz	counter		; count down to zero
 	bra	loop		; keep going until finished
 	
