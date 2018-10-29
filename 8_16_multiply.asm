ecb48ea0c1c745e5bfbc9651e2f52bc32477b422
ep1616 <ep1616@skbl-422-013.ph.ic.ac.uk>
2018-10-29 16:09:52 +0000

    working 8 by 16 bit multiplier

--- 8_16_multiply.asm
+++ 8_16_multiply.asm
@@ -1,15 +1,42 @@
 #include p18f87k22.inc
-    
 
+	global  multiply816
+
+acs0	udata_acs   ; reserve data space in access ram
 highbyte    res 1   ; reserve one byte for a high byte variable
 lowbyte	    res 1   ; reserve one byte for a low byte variable
-    
-_8_16_multiply	; multiply 0x04D2 by 0x8A
+
+highprod    res 1 	    
+middleprod  res 1
+lowprod	    res 1
+
+	    
+main	code
+	
+	
+	    
+multiply816 ; 0x04D2 times 0x8A	
     movlw   0x04
-    movwf   highbyte
+    movwf   highbyte    ;high
     movlw   0xD2
-    movwf   lowbyte
+    movwf   lowbyte    ;low
     
     movlw   0x8A
-    mulwf   lowbyte
+    mulwf   lowbyte	    ;multiply W(0x8A) by lowbyte
+    movff   PRODL, lowprod
+    nop
+    movff   PRODH, middleprod
+    nop
+    
+    movlw   0x8A
+    mulwf   highbyte	    ;multiply W(0x8A) by highbyte
+    movf    PRODL, 0, 0	    ;move the lower byte of the multiplication to W
+    addwfc  middleprod	    ;add W to middleprod
+    nop
+    movff   PRODH, highprod
+    nop
+
+    return
+    
+    end
     
