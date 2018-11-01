9d2d2261d243b31248dbfd49a4b79016f7749dc1
ep1616 <ep1616@skbl-422-013.ph.ic.ac.uk>
2018-11-01 14:48:17 +0000

    working progress for 8 by 24 multiplier. Error in compiling due to multiply816

--- 8_16_multiply.asm
+++ 8_16_multiply.asm
@@ -1,14 +1,20 @@
 #include p18f87k22.inc
 
-	global  multiply816, multiply1616
+    global  multiply816, multiply1616
 
 acs0	udata_acs   ; reserve data space in access ram
 highbyte    res 1   ; reserve one byte for a high byte variable
 lowbyte	    res 1   ; reserve one byte for a low byte variable
 ; 8 by 16 product results
-highp   res 1 	    
-middlep  res 1
-lowp    res 1
+highp	    res 1 	    
+middlep	    res 1
+lowp	    res 1
+; 8 by 24 product results
+upperpr	    res 1
+highpr	    res 1 	    
+middlepr    res 1
+lowpr	    res 1    
+   
 ;16 by 16 product result (32 bit)
 upperprod   res 1
 highprod    res 1 	    
@@ -25,7 +31,7 @@ num_2_low   res 1
    
 main	code	    
 
-number_setup
+number_setup_16_16
     movlw   0x41	
     movwf   num_1_high
     movlw   0x8A
@@ -34,16 +40,63 @@ number_setup
     movwf   num_2_high
     movlw   0xD2
     movwf   num_2_low
+    return
     
 
+  
+number_setup_8_24
+    movlw   0x41	
+    movwf   num_1
+    movlw   0x8A
+    movwf   num_2_low
+    movlw   0x04
+    movwf   num_2_middle
+    movlw   0xD2
+    movwf   num_2_high
+    return
+    
+ 
+/*multiply824    ;16 bit by 16 bit multiplier
+    movlw   0x0
+    movwf   upperpr
+    movf    num_1, 0, 0
+    mulwf   num_2_low	    ;multiply W by lowbyte
+    movff   PRODL, lowpr
+    nop
+    movff   PRODH, middlepr
+    nop
+    
+    movf    num_1, 0, 0
+    mulwf   num_2_middle	    ;multiply W by lowbyte
+    movf    PRODL,  0, 0
+    addwfc  middlepr
+    nop
+    movf    PRODH,  0, 0
+    addwfc  highpr
+    nop
+    
+    movf    num_1, 0, 0
+    mulwf   num_2_high	    ;multiply W by lowbyte
+    movff   PRODL,  0, 0
+    addwfc  highpr
+    nop
+    movff   PRODL,  0, 0
+    addwfc  upperpr
+    nop
+*/
+    
+        
+
+    
 multiply1616	    ;16 bit by 16 bit multiplier
+    call    number_setup_16_16
     movff   num_1_low, _8bit_num
     call    multiply816
     movff   lowp, lowprod
     movff   middlep, middleprod
     movff   highp, highprod
 
-    movlw   0x00
+    movlw   0x00	    ; reset upper prod to 0x00
     movwf   upperprod
     
     movff   num_1_high, _8bit_num
@@ -71,6 +124,13 @@ multiply816 ;multiply an 8 bit by a 16 bit
     nop
 
     return
+    
+    
+    
+    
+    
+    
+    
     
     end
     
